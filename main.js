3a64214471525892d6bc8128e61d962d327309a0
sONg20NOW <sgk1004s@naver.com>
2021-10-03 18:21:24 +0900

    practice

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-//let arr = [1, 2, 3, "apple"];
-//console.log(typeof arr)
-//let a = 1;
-//console.log(typeof a);
-console.log("hi");
+let sum = (a, b) => a + b;
+
+let add = sum;
+console.log(typeof add);
+console.log(typeof sum);
+sum["1"] = 1;
+console.log(sum[1]);
+console.log(add[1]);
+
+let x = [1, 2, 3, 4, 5];
+y = x.map((n) => n + 1);
+console.log("y: ", y);
+console.log("x: ", x);
+
+z = x.filter((n) => n % 2 === 0);
+console.log("z: ", z);
+console.log("x: ", x);
+
+x.forEach((f) => {
+    console.log(f);
+});
+
+let xx = x.filter((n) => n % 2 === 0);
+console.log("xx: ", xx);
